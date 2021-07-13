159b745135e6c41f58941345a289cba59c84eae1
sumin98214 <jsm6616@naver.com>
2021-07-13 17:05:45 +0900

    dbdbdb

---
+++ 6_DB/dbdbdeep.py
@@ -0,0 +1,18 @@
+import pymysql as db
+
+local = 'dbtest.cuslvraxrcdc.ap-northeast-2.rds.amazonaws.com'
+con = db.connect(
+    host=local,g
+    user='user',
+    password='qwert123',
+    charset='utf8'
+)
+cur = con.cursor()
+query = "SELECT * FROM test"
+try:
+    cur.execute(query)
+except db.Error as e:
+    print(e)
+finally:
+    cur.close()
+    con.close()
