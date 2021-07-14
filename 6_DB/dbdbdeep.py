22b1827141a6aa3c32077d0036b5edb937ceecaf
sumin98214 <jsm6616@naver.com>
2021-07-14 19:46:38 +0900

    changh db.py

--- 6_DB/dbdbdeep.py
+++ 6_DB/dbdbdeep.py
@@ -1,20 +1,3 @@
-import pymysql as db
+import product_table as pro
 
-local = 'dbtest.cuslvraxrcdc.ap-northeast-2.rds.amazonaws.com'
-con = db.connect(
-    host=local,
-    user='user',
-    db='ttockshow',
-    password='qwert123',
-    charset='utf8'
-)
-cur = con.cursor()
-
-query = "SELECT * FROM PRODUCT"
-try:
-    cur.execute(query)
-except db.Error as e:
-    print(e)
-finally:
-    cur.close()
-    con.close()
+pro.cre_proT()
--- 6_DB/product_table.py
+++ 6_DB/product_table.py
@@ -1,29 +1,26 @@
-import pymysql as db
+import con_db
 
-# CONNECT_DB
-local = 'dbtest.cuslvraxrcdc.ap-northeast-2.rds.amazonaws.com'
-con = db.connect(
-    host=local,
-    user='user',
-    db='ttockshow',
-    password='qwert123',
-    charset='utf8'
-)
-cur = con.cursor()
-
+cur = con_db.connect()
 # CREATE_PRO
-cre_pro = """CREATE TABLE pro (
+cre_productT = """CREATE TABLE pro (
 barcord_id INT,
 name VARCHAR(255) NOT NULL,
 image_link VARCHAR(255) DEFAULT 'None',
 count INT DEFAULT 0,
-PRIMARY KEY (barcord_id) )"""
+PRIMARY KEY (barcord_id) 
+)"""
 
 # DELETE TABLE
-del_pro = "DROP TABLE PRO"
+del_productT = "DROP TABLE pro"
 
-def c_pro():
-    cur.execute(cre_pro)
 
-con.commit()
-cur.close()
+# CREATE_FUN
+def cre_proT():
+    cur.execute(cre_productT)
+    con_db.commit()
+
+
+# DELETE_FUN
+def del_proT():
+    cur.execute(del_productT)
+    con_db.commit()
