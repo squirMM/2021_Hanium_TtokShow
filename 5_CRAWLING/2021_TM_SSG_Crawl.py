b3ae540e18fd236ac9cd4b7d441b3fe7227b9f15
김지석 <plays0708@naver.com>
2021-07-25 20:54:15 +0900

    번호, 이름, 평점, 리뷰, 날짜 튜플 형식으로 긁어오기 완료

--- 5_CRAWLING/2021_TM_SSG_Crawl.py
+++ 5_CRAWLING/2021_TM_SSG_Crawl.py
@@ -17,7 +17,7 @@ url = f'http://www.ssg.com/search.ssg?target=all&query={plusUrl}'
 driver.get(url)
 
 driver.find_element_by_css_selector('.thmb').click()
-time.sleep(2)
+time.sleep(1)
 
 review_total = driver.find_element_by_css_selector('.num').text
 print("리뷰 개수:",review_total)
@@ -37,24 +37,27 @@ print("상품명:",product)
 review_grade = driver.find_element_by_css_selector('.cdtl_grade_total').text
 print("평점:", review_grade)
 
-
 def get_page_data(): 
+    numbers = driver.find_elements_by_css_selector('.number') #번호 수집
     users = driver.find_elements_by_css_selector('.user') # 사용자명 수집 
     ratings = driver.find_elements_by_css_selector('.sp_cdtl.cdtl_cmt_per') # 평점 수집 
     reviews = driver.find_elements_by_css_selector('.cdtl_cmt_tx.v2') #리뷰 수집
-
-    # 사용자명수와 평점수가 같을 경우만 수집 
+    
+    # 리뷰개수와 평점수가 같을 경우만 수집 
     if len(reviews) == len(ratings):
         for i in range(len(ratings)):
+            number = numbers[i+1].text
             user = users[i+1].text
             rating = ratings[i].text
             rating = rating.replace("구매 고객 평점 별 5개 중 ","")
             rating = rating.replace("개","")
             rating = int(rating)
             review = reviews[i].text
-            review = review.replace("사진" , "")
-            review = review.replace("\n" , "")
-            data = (user, rating, review)
+            review = review.replace("사진\n" , "")
+            num = (2*i+1) % 20
+            date = driver.find_element_by_xpath(f'//*[@id="cdtl_cmt_tbody"]/tr[{num}]/td[5]/div')
+            date = date.text.replace("-","")
+            data = (int(number), user, int(rating), review, int(date))
             data_list.append(data)
             print(data)
 print("수집 시작") # 첫 페이지 수집하고 시작 
@@ -62,20 +65,19 @@ print("수집 시작") # 첫 페이지 수집하고 시작
 get_page_data() # 버튼을 눌러서 페이지를 이동해 가면서 계속 수집. # 예외처리를 해줘야 함. 하지 않으면 중지됨. 
 print("1 page 수집 끝")
 driver.find_element_by_xpath('//*[@id="comment_navi_area"]/a[1]').click() 
-for page in range(1, total_page): 
+for page in range(2, total_page): 
     try:
         get_page_data()
-        if page >= 1 and page < 11:
+        if page < 11:
             print(str(page) + " page 수집 끝") 
             button_index = page + 1  # 데이터 수집이 끝난 뒤 다음 페이지 버튼을 클릭 
             driver.find_element_by_xpath(f'//*[@id="comment_navi_area"]/a[{button_index}]').click() 
-            time.sleep(1)  
+            time.sleep(1)
         else:
             print(str(page+1) + " page 수집 끝") 
             button_index = page % 10 + 3 # 데이터 수집이 끝난 뒤 다음 페이지 버튼을 클릭 
             driver.find_element_by_xpath(f'//*[@id="comment_navi_area"]/a[{button_index}]').click() 
             time.sleep(1) 
-
 
     except: 
         print("수집 에러") 
