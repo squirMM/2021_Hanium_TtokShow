9e05165610f858bf9ec02673baff3ec96b98fa2c
sumin98214 <jsm6616@naver.com>
2021-08-17 23:11:29 +0900

    쓸데 없는거

--- 8_APP/app/src/main/java/com/example/test_ttokshow/MainActivity.java
+++ 8_APP/app/src/main/java/com/example/test_ttokshow/MainActivity.java
@@ -59,12 +59,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         list_s = new ArrayList<>();
         list = new ArrayList<>();
-        ImageView imageView1;
-        imageView1 = (ImageView) findViewById(R.id.imageView11);
-        imageView1.setImageResource(R.drawable.aver3);
 
 
         new Thread(){
+            @Override
             public void run(){
                 Client.main();
                 String[] out = Client.getOutput();
