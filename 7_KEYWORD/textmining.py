c9a0e499692394f29b19155e57416b7f017b7032
110w110 <110w110@naver.com>
2021-08-19 15:13:09 +0900

    텍스트마이닝 완성

--- 7_KEYWORD/textmining.py
+++ 7_KEYWORD/textmining.py
@@ -2,14 +2,38 @@ from konlpy.tag import Okt
 import sys
 from collections import Counter
 from wordcloud import WordCloud
+import pymysql as db
+import matplotlib.pyplot as plt
+
+local = 'dbtest.cuslvraxrcdc.ap-northeast-2.rds.amazonaws.com'
+con = db.connect(
+    host=local,
+    user='user',
+    db='ttockshow',
+    password='qwert123',
+    charset='utf8'
+)
+
+cur=con.cursor()
+
+def fetchReview(codeNum):
+    police = """select contents from review where barcord_id = %s"""
+    cur.execute(police, [codeNum])
+    dontknow = list(cur.fetchall())
+    return dontknow
 
 def get_noun(news):
     okt = Okt()
-    noun = okt.nouns(news)
+    noun = okt.phrases(news)
+    print("1")
     for i,v in enumerate(noun):
-        if len(v) < 2:
+        if len(v) < 10:
             noun.pop(i)
+
+    print("2")
     count = Counter(noun)
+
+    print("3")
     noun_list = count.most_common(100)
 
     return noun_list
@@ -20,8 +44,14 @@ def visualize(noun_list):
     wc.to_file('keyword.png')
 
 if __name__=="__main__":
-    filename = './data.txt'
-    f=open(filename,'r',encoding='utf-8')
-    news=f.read()
-    noun_list = get_noun(news)
-    visualize(noun_list)
+    review_list = [x[0] for x in fetchReview(str(8801007160337))]
+    print("fetch done.")
+    #print(review_list)
+    strReview = " ".join(review_list)
+    print("string done.")
+    #print(strReview)
+    inputlist = strReview[-100000:-1]
+    print(inputlist)
+    visualize(get_noun(inputlist))
+    #newVisualize(get_noun(strReview))
+    print("visualization done. :)")
